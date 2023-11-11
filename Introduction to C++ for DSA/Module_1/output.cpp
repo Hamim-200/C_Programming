7b4e73709724aee88690f335bc475d3224ca462c
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-11-11 23:15:28 +0600

    cpp day-1

---
+++ Introduction to C++ for DSA/Module_1/output.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+    //std::cout<<"Hello World";
+
+    int a=22;
+    long long int b = 10000000000000;
+    float c = 1.5;
+    double d= 1.5378343;
+    char ch = 'A';
+
+    // std::cout<<a;
+    std::cout<< "My Favourite Number is "<<a<<" I Also love him";
+
+    // New Line
+    std::cout<< "My Name Is Hamim" << "\n"<< "I am "<<a<<" Years Old";
+    std::cout<< "My Name Is Hamim" << std::endl<< "I am "<<a<<" Years Old";
+    return 0;
+}
