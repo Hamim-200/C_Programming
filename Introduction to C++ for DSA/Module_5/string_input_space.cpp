fc7f6669717818afc93824314b4d054458d8ddb4
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-11-21 21:23:53 +0600

    module 5 done

---
+++ Introduction to C++ for DSA/Module_5/string_input_space.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    cin.ignore();
+
+    string s;
+    //cin>>s; //Without Space
+    getline(cin,s);
+
+    cout<<x<<endl;
+    cout<<s<<endl;
+
+
+
+
+    return 0;
+}
