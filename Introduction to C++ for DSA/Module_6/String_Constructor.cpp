71570d62f202b61f08bfe94a9dff466e1d4408f7
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-11-22 01:00:48 +0600

    module-6 half

---
+++ Introduction to C++ for DSA/Module_6/String_Constructor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s ="Hello world";
+
+    //1-Print Full String
+    // string s("Hello World");
+    
+    //2-Print first 7 string
+    //string s("Hello world",7);
+
+    //3-Print from after 4 string
+    // string a="hello world";
+    // string s(a,4);
+
+    //4-Print same string
+    //string s(10,'I');
+
+    cout<<s<<endl;
+    return 0;
+}
