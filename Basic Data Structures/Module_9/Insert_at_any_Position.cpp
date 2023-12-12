2d9dfb59d1f648861216317615609fdaccb0638d
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-12-12 14:29:26 +0600

    module-9-(0-4)

---
+++ Basic Data Structures/Module_9/Insert_at_any_Position.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_front(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_any_position(Node *head, int pos, int val)
+{
+    // Create New Node
+    Node *newNode = new Node(val);
+    // Keep Head in tmp ariable
+    Node *tmp = head;
+    // Go to previous position where i want to change
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next; //---->20 after 20 i want to set new value
+    }
+    // Set new node next value
+    newNode->next = tmp->next;     // 100->30
+    tmp->next = newNode;           // 20->100
+    newNode->next->prev = newNode; // 100<-30
+    newNode->prev = tmp;           // 20<-100
+}
+int size(Node *head)
+{
+    Node *tmp = head;
+    int cnt = 0;
+    while (tmp != NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *tail = c;
+
+    // connection
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+
+    int pos, val;
+    cin >> pos >> val;
+    if (pos >= size(head))
+    {
+        cout << "Invalid" << endl;
+    }
+    else
+    {
+        insert_at_any_position(head, pos, val);
+    }
+    print_front(head);
+    print_reverse(tail);
+    return 0;
+}
